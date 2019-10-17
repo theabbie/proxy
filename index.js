eeedad7de602fa7b81dee537887637f086f7da20
abhishek <abhishek7gg7@gmail.com>
2019-10-17 22:51:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var app = require('express')();
-
-app.get("/*", function(req,res) {
-
-})
+var express  = require('express');
+var app      = express();
+var httpProxy = require('http-proxy');
+var apiProxy = httpProxy.createProxyServer();
+ 
+app.all("/*", function(req, res) {
+apiProxy.web(req, res, {target: "https://google.com"});
+});
 
 app.listen(process.env.PORT);
