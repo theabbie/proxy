85ed457cc2a088185ed88e8538c5d00507167b6c
abhishek <abhishek7gg7@gmail.com>
2019-10-18 21:29:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express  = require('express');
 var app      = express();
 var httpProxy = require('http-proxy');
-var apiProxy = httpProxy.createProxyServer();
+var apiProxy = httpProxy.createProxyServer({secure:false});
  
 app.all("/*", function(req, res) {
 apiProxy.web(req, res, {target: req.query.url});
