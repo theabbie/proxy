0e43e6bfa7b6c9a6bef621c8d6db31ff57d0a020
abhishek <abhishek7gg7@gmail.com>
2019-10-18 21:35:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express  = require('express');
 var app      = express();
 var httpProxy = require('http-proxy');
-var apiProxy = httpProxy.createProxyServer({secure:false});
+var apiProxy = httpProxy.createProxyServer();
  
 app.all("/*", function(req, res) {
-apiProxy.web(req, res, {target: req.query.url});
+apiProxy.web(req, res, {target: req.query.url,changeOrigin: true});
 });
 
 app.listen(process.env.PORT);
